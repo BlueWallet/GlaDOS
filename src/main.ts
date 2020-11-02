6d0fe36f3b0f84ba05df6b2f64ba4f7280420287
Overtorment <overtorment@gmail.com>
2020-11-02 13:56:05 +0000

    REF

--- src/main.ts
+++ src/main.ts
@@ -1,120 +1,123 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-const g = process.env.TOKEN;
-if (!g) {
-    console.error('no TOKEN env provided');
-    process.exit(1);
+const token = process.env.TOKEN;
+if (!token) {
+  console.error("no TOKEN env provided");
+  process.exit(1);
 }
-
-
 
 async function run(): Promise<void> {
-    const octokit = github.getOctokit(g)
+  const octokit = github.getOctokit(token);
 
-    try {
-        const {data: pullRequests} = await octokit.pulls.list({
-            repo: 'BlueWallet',
-            owner:'BlueWallet',
-            state: 'open'
-        });
+  try {
+    const { data: pullRequests } = await octokit.pulls.list({
+      repo: "BlueWallet",
+      owner: "BlueWallet",
+      state: "open",
+    });
 
+    for (const pr of pullRequests) {
+      console.log(`${pr.title} (${pr.number})`);
 
-        for (const pr of pullRequests) {
-            // if (pr.number !== 2021) continue;
-            console.log(`${pr.title} (${pr.number})`, );
+      let approved = false;
+      let e2ePassed = false;
+      let unitTestsPassed = false;
+      let thereAreBlockerLabels = false;
+      let outsiderContributor = pr.head.repo.full_name !== "BlueWallet/BlueWallet";
 
-            let approved = false;
-            let e2ePassed = false;
-            let unitTestsPassed = false;
-            let thereAreBlockerLabels = false;
-            let outsiderContributor = pr.head.repo.full_name !== 'BlueWallet/BlueWallet';
+      for (const label of pr.labels) {
+        if (label.name.toUpperCase().startsWith("DO NOT MERGE") || label.name.toUpperCase().startsWith("WIP")) {
+          thereAreBlockerLabels = true;
+        }
+      }
 
+      const reviews = await octokit.request("GET /repos/{owner}/{repo}/pulls/{ref}/reviews", {
+          repo: "BlueWallet",
+          owner: "BlueWallet",
+          ref: pr.number,
+      });
 
-            for (const label of pr.labels) {
-                if (label.name.toUpperCase().startsWith('DO NOT MERGE') || label.name.toUpperCase().startsWith('WIP')) {
-                    thereAreBlockerLabels = true;
-                }
-            }
+      const checks = await octokit.checks.listForRef({
+        repo: "BlueWallet",
+        owner: "BlueWallet",
+        ref: pr.head.sha,
+      });
 
-            const reviews = await octokit.request('GET /repos/{owner}/{repo}/pulls/{ref}/reviews', {
-                repo: 'BlueWallet',
-                owner:'BlueWallet',
-                ref: pr.number
-            });
+      for (const check of checks.data.check_runs) {
+        if (check.name.startsWith("Travis CI") && check.conclusion === "success") e2ePassed = true;
+      }
 
-            const checks = await octokit.checks.listForRef({
-                    repo: 'BlueWallet',
-                    owner:'BlueWallet',
-                    ref: pr.head.sha,
-                });
-
-            for (const check of checks.data.check_runs) {
-                if (check.name.startsWith('Travis CI') && check.conclusion === 'success') e2ePassed = true;
-            }
-
-
-            if (reviews.data.length >= 1) {
-                let _approves = {};
-                for (const review of reviews.data) {
-                    if (review['state'] !== 'APPROVED') {
-                        console.log('NOT approved by', review.user.login);
-                        _approves[review.user.login] = false;
-                    } else {
-                        console.log('approved by', review.user.login);
-                        _approves[review.user.login] = true;
-                    }
-                }
-
-                approved = (Object.values(_approves).filter((el) => el === false).length === 0);
-            }
-
-            if (pr.requested_reviewers.length !== 0) {
-                console.log('not all requested reviews are done');
-                approved = false;
-            }
-
-
-            const statuses = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}/status', {
-                repo: 'BlueWallet',
-                owner:'BlueWallet',
-                ref: pr.head.sha
-            });
-
-            for (const status of statuses.data.statuses) {
-                if (status.context.startsWith('ci/circleci') && status.state === 'success') unitTestsPassed = true;
-            }
-
-            console.log({approved, e2ePassed, unitTestsPassed, outsiderContributor, thereAreBlockerLabels});
-
-            if (approved && e2ePassed && unitTestsPassed && !outsiderContributor && !thereAreBlockerLabels) {
-                console.log('LGTM. lets merge');
-                // continue; // fixme
-
-                const mergeResult = await octokit.pulls.merge({
-                    repo: 'BlueWallet',
-                    owner:'BlueWallet',
-                    pull_number: pr.number,
-                })
-
-                if (mergeResult.data.message.indexOf('successfully') !== -1) {
-                    console.log({mergeResult});
-                    await octokit.issues.createComment({
-                        repo: 'BlueWallet',
-                        owner: 'BlueWallet',
-                        issue_number: pr.number,
-                        body: 'Unbelievable! You, Subject Name Here, must be the pride of Subject Hometown Here.',
-                    });
-                }
-            }
-
-            console.log('=======================================================\n');
+      if (reviews.data.length >= 1) {
+        let _approves = {};
+        for (const review of reviews.data) {
+          if (review["state"] !== "APPROVED") {
+            console.log("NOT approved by", review.user.login);
+            _approves[review.user.login] = false;
+          } else {
+            console.log("approved by", review.user.login);
+            _approves[review.user.login] = true;
+          }
         }
 
-    } catch (error) {
-        console.warn(error);
-    }
-}
+        approved = Object.values(_approves).filter((el) => el === false).length === 0;
+      }
 
+      if (pr.requested_reviewers.length !== 0) {
+        console.log("not all requested reviews are done");
+        approved = false;
+      }
+
+      const statuses = await octokit.request(
+        "GET /repos/{owner}/{repo}/commits/{ref}/status",
+        {
+          repo: "BlueWallet",
+          owner: "BlueWallet",
+          ref: pr.head.sha,
+        }
+      );
+
+      for (const status of statuses.data.statuses) {
+        if (status.context.startsWith("ci/circleci") && status.state === "success") unitTestsPassed = true;
+      }
+
+      console.log({
+        approved,
+        e2ePassed,
+        unitTestsPassed,
+        outsiderContributor,
+        thereAreBlockerLabels,
+      });
+
+      if (approved && e2ePassed && unitTestsPassed && !outsiderContributor && !thereAreBlockerLabels) {
+        console.log("LGTM. lets merge");
+        // continue; // fixme
+
+        const mergeResult = await octokit.pulls.merge({
+          repo: "BlueWallet",
+          owner: "BlueWallet",
+          pull_number: pr.number,
+        });
+
+        let body = "I could not merge it.";
+        if (mergeResult.data.message.indexOf("successfully") !== -1) {
+          console.log({ mergeResult });
+          body = "Unbelievable. You, [subject name here], must be the pride of [subject hometown here]!";
+        }
+
+        await octokit.issues.createComment({
+          repo: "BlueWallet",
+          owner: "BlueWallet",
+          issue_number: pr.number,
+          body,
+        });
+      }
+
+      console.log("=======================================================\n");
+    }
+  } catch (error) {
+    console.warn(error);
+  }
+}
 
 run();

2364d7ea3e2873c69ca87550e8a4ae805eb002e4
Marcos Rodriguez Vélez <marcospr@pm.me>
2024-11-03 16:37:43 -0400

    Update main.ts
    Made no sense to include it in the first place

--- src/main.ts
+++ src/main.ts
@@ -65,8 +65,7 @@ async function run(): Promise<void> {
     for (const label of pr.labels) {
       if (
         label.name.toUpperCase().startsWith("DO NOT MERGE") ||
-        label.name.toUpperCase().startsWith("WIP") || 
-        label.name.toUpperCase().startsWith("TESTFLIGHT")
+        label.name.toUpperCase().startsWith("WIP")
       ) {
         thereAreBlockerLabels = true;
       }
